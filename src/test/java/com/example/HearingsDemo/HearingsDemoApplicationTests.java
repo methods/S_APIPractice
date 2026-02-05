3511bcaed9cabf2d5b0c3080abca6b8a6b6d2fc4
codesungrape <miss.s.rai@outlook.com>
2026-02-05 14:39:20 +0000

    Initial commit

---
+++ src/test/java/com/example/HearingsDemo/HearingsDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.HearingsDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HearingsDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
