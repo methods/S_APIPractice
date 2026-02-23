516eb39aa840c99eb1f54cb48a87457aa499dcde
codesungrape <miss.s.rai@outlook.com>
2026-02-23 18:03:42 +0000

    Add PersonRepository interface; fix TDD

---
+++ src/main/java/com/example/HearingsDemo/domain/PersonRepository.java
@@ -0,0 +1,17 @@
+package com.example.HearingsDemo.domain;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+// 1. @Repository: Tells Spring "this bean talks to the DB"
+// 2. extends JpaRepository: inherits all methods from this class
+// 3. <Person, PersonId>: The Generics:
+//          - First: the Entity we are managing.
+//          - Second: the Data Type of the Primary Key 
+//          (Because we have a Composite Key, we MUST put 'PersonId' here, not 'UUID')
+
+@Repository
+public interface PersonRepository extends JpaRepository<Person, PersonId> {
+
+    
+}
