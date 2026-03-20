0cca3d4d82357eaf87f8bb59104249970a7de542
codesungrape <miss.s.rai@outlook.com>
2026-03-20 12:31:48 +0000

    docs: fix typo + type mismatch in OpenApi DTO exmaple

--- src/main/java/com/example/HearingsDemo/person/PersonCollectionResponseDTO.java
+++ src/main/java/com/example/HearingsDemo/person/PersonCollectionResponseDTO.java
@@ -1,11 +1,15 @@
 package com.example.HearingsDemo.person;
 
+import io.swagger.v3.oas.annotations.media.ArraySchema;
 import io.swagger.v3.oas.annotations.media.Schema;
 
 import java.util.List;
 
 public record PersonCollectionResponseDTO(
-    @Schema(description = "List of persons found for the hearing", example = "[]")
+    @Schema(
+        description = "List of persons found for the hearing",
+        example = "[{\"personUuid\": \"550e8400-e29b-41d4-a716-446655440000\", \"firstName\": \"John\", \"lastName\": \"Doe\"}]"
+    )
     List<PersonResponseDTO> persons,
 
     @Schema(description = "The total number of persons associated with this hearing", example = "5")
