5517d3102dba8b69bc43e55e9999bb931b8f6b20
codesungrape <miss.s.rai@outlook.com>
2026-04-01 09:37:49 +0100

    feat(defendant-gob-accounts): add collection endpoint for hearing lookups
    - Implement GET /api/v1/defendant-gob-accounts using @RequestParam filters.
    - Add MockMvc tests for 200 OK wiht data and empty collection scenarios.
    - Verify correct JSON array mapping and JsonPath assertions for List results.

--- src/main/java/com/example/HearingsDemo/defendantgobaccounts/DefendantGOBAccountsController.java
+++ src/main/java/com/example/HearingsDemo/defendantgobaccounts/DefendantGOBAccountsController.java
@@ -4,11 +4,9 @@ package com.example.HearingsDemo.defendantgobaccounts;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 import java.util.UUID;
 
 @Tag(name = "Defendant GOB Accounts Viewstore", description = "Query Endpoints for fetching associated accounts")
@@ -31,5 +29,17 @@ public class DefendantGOBAccountsController {
         DefendantGOBAccountDTO accountDto = service.getAccountByIds(masterDefendantId, accountCorrelationId);
 
         return ResponseEntity.ok(accountDto);
+    }
+
+    @Operation(summary = "Get all associated account accounts", description = "Retrieves by masterDefendantId and " +
+        "hearingId")
+    @GetMapping
+    public ResponseEntity<List<DefendantGOBAccountDTO>> getAccounts(
+        @RequestParam UUID masterDefendantId,
+        @RequestParam UUID hearingId
+    ) {
+        List<DefendantGOBAccountDTO> accounts = service.getAllAccountsForHearing(masterDefendantId, hearingId);
+
+        return ResponseEntity.ok(accounts);
     }
 }
--- src/test/java/com/example/HearingsDemo/defendantgobaccounts/DefendantGOBAccountsControllerTest.java
+++ src/test/java/com/example/HearingsDemo/defendantgobaccounts/DefendantGOBAccountsControllerTest.java
@@ -11,6 +11,7 @@ import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 
 import java.time.LocalDateTime;
+import java.util.List;
 import java.util.UUID;
 
 import static org.mockito.Mockito.when;
@@ -53,11 +54,13 @@ public class DefendantGOBAccountsControllerTest {
                 masterDefendantId, accountCorrelationId))
             .andExpect(status().isOk())
             .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+
             // Identifiers
             .andExpect(jsonPath("$.masterDefendantId").value(masterDefendantId.toString()))
             .andExpect(jsonPath("$.accountCorrelationId").value(accountCorrelationId.toString()))
             .andExpect(jsonPath("$.hearingId").value(hearingId.toString()))
             .andExpect(jsonPath("$.accountRequestTime").value(accountRequestTime.toString()))
+
             // String values
             .andExpect(jsonPath("$.accountNumber").value(mockDto.accountNumber()))
             .andExpect(jsonPath("$.caseReferences").value(mockDto.caseReferences()));
@@ -79,6 +82,64 @@ public class DefendantGOBAccountsControllerTest {
         mockMvc.perform(get("/api/v1/defendant-gob-accounts//{masterDefendantId}/correlations/{accountCorrelationId}",
                 masterDefendantId, accountCorrelationId))
             .andExpect(status().isNotFound());
+
+    }
+
+    // =======================================================
+    // Index Collections lookup
+    // =======================================================
+
+    @Test
+    @DisplayName("GET /api/v1/defendant-gob-accounts should return a 200 OK with a list of account dtos associated with a hearingId when found")
+    void shouldReturnOKWithDtosWhenFound() throws Exception {
+        // Arrange
+        UUID masterId = UUID.randomUUID();
+        UUID hearingId = UUID.randomUUID();
+        UUID correlationId1 = UUID.randomUUID();
+        UUID correlationId2 = UUID.randomUUID();
+        LocalDateTime now = LocalDateTime.now();
+
+        List<DefendantGOBAccountDTO> mockDtos = List.of(
+            new DefendantGOBAccountDTO(masterId, correlationId1, hearingId, "0745", now, "Test Ref 1"),
+            new DefendantGOBAccountDTO(masterId, correlationId2, hearingId, "034", now, "Test Ref 2")
+        );
+
+        when(service.getAllAccountsForHearing(masterId, hearingId)).thenReturn(mockDtos);
+
+        // Act & Assert
+        mockMvc.perform(get("/api/v1/defendant-gob-accounts")
+                .param("masterDefendantId", masterId.toString())
+                .param("hearingId", hearingId.toString()))
+            .andExpect(status().isOk())
+            .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+            .andExpect(jsonPath("$.length()").value(2))
+            .andExpect(jsonPath("$[0].accountNumber").value("0745"))
+
+            .andExpect(jsonPath("$[0].accountCorrelationId").value(correlationId1.toString()))
+            // 3. Verify the second element data
+            .andExpect(jsonPath("$[1].accountNumber").value("034"))
+            .andExpect(jsonPath("$[1].accountCorrelationId").value(correlationId2.toString()));
+    }
+
+    @Test
+    @DisplayName("GET /api/v1/defendant-gob-accounts should return 200 OK with an empty list when no accounts are " +
+        "found")
+    void shouldReturnOKWithEmptyListWhenNotFound() throws Exception {
+
+        // Arrange
+        UUID masterId = UUID.randomUUID();
+        UUID hearingId = UUID.randomUUID();
+
+        when(service.getAllAccountsForHearing(masterId, hearingId)).thenReturn(List.of());
+
+        // Act & assert
+        mockMvc.perform(get("/api/v1/defendant-gob-accounts")
+                .param("masterDefendantId", masterId.toString())
+                .param("hearingId", hearingId.toString()))
+            .andExpect(status().isOk())
+            .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+            // Check that the root element is empty
+            .andExpect(jsonPath("$").isEmpty());
 
     }
 }
